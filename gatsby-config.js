d7473ba7e0b915cb32af3268b6f074905fad676f
vivihzg <vivihzgithub@gmail.com>
2024-03-26 16:14:39 -0300

    finalizado

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,11 +14,36 @@ module.exports = {
     },
     __key: "images"
   }, {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "pages",
+        "path": "./src/pages/"
+      },
+
+      __key: "pages"
+
     },
-    __key: "pages"
-  }]
+    {
+      resolve: `gatsby-plugin-web-font-loader`,
+        options: {
+          google: {
+            families: [`Montserrat:100,200,300,400,500,600,700,800`, `sans-serif`,`Paytone One`]
+          }
+        }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Quita Simples`,
+        short_name: `Quita Simples`,
+        description: `Compramos seu Precatório, não fazemos você esperar, seu pagamento aqui é à Vista!.`,
+        start_url: `/`,
+        background_color: `var(--color-background)`,
+        theme_color: `var(--color-primary)`,
+        icon: `src/images/icon.png`,
+        lang: `pt-br`,
+        display: `standalone`,
+      },
+    },
+  ]
 };

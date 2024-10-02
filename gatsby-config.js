e817d295c24b1ad9837f87acaeac64d686ac868a
vivihzg <vivihzgithub@gmail.com>
2024-10-02 10:52:39 -0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,20 @@ module.exports = {
         }
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-9QCD0ER9NB", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Quita Simples – Simplificando o pagamento dos seus precatórios`,

27526d018668f29ff4e08a6d1d53db40f56b4a3a
Maria Vitoria <vivihzgithub@gmail.com>
2026-04-24 10:26:21 -0300

    adicionando analytics ahref

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,15 +4,9 @@ exports.onRenderBody = ({ setHeadComponents }) => {
   setHeadComponents([
     React.createElement("script", {
       key: "ahrefs-analytics",
-      dangerouslySetInnerHTML: {
-        __html: `
-          var ahrefs_analytics_script = document.createElement('script');
-          ahrefs_analytics_script.async = true;
-          ahrefs_analytics_script.src = 'https://analytics.ahrefs.com/analytics.js';
-          ahrefs_analytics_script.setAttribute('data-key', 'jibhCjTFTFfD7K4i/Viv4g');
-          document.getElementsByTagName('head')[0].appendChild(ahrefs_analytics_script);
-        `,
-      },
+      src: "https://analytics.ahrefs.com/analytics.js",
+      "data-key": "jibhCjTFTFfD7K4i/Viv4g",
+      async: true,
     }),
   ])
 }
